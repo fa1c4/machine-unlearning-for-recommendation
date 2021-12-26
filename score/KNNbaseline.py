33f0c95d692cf4eeac506ab4017dfd197c9812f8
falca <azesinter@mail.ustc.edu.cn>
2021-12-26 11:16:22 +0800

    implemented prediction of score with a test dataset on SVD and KNN

---
+++ score/KNNbaseline.py
@@ -0,0 +1,151 @@
+import os
+import pandas as pd
+import time
+from surprise import Dataset, KNNBaseline, Reader
+from surprise.model_selection import cross_validate
+import random
+
+
+class KNNbase_Unlearning():
+
+    def __init__(self):
+        self.user_total_num = 943
+        self.RMSE, self.MAE = [], []
+        self.reader, self.data, self.data_df, self.item_df, self.item_dict = None, None, None, None, None
+        self.user_based_sim_option, self.item_based_sim_option = None, None
+
+    def data_readin(self, path_to_udata):
+        # 协同过滤方法
+        # 或载入本地数据集# 数据集路径 path to dataset file
+        file_path = os.path.expanduser(path_to_udata)
+        # 使用Reader指定文本格式，参数line_format指定特征（列名），参数sep指定分隔符
+        self.reader = Reader(line_format='user item rating timestamp', sep='\t')
+
+        # 加载数据集
+        self.data = Dataset.load_from_file(file_path, reader=self.reader)
+        self.data_df = pd.read_csv(file_path, sep='\t', header=None, names=['user', 'item', 'rating', 'timestamp'])
+        self.item_df = pd.read_csv(os.path.expanduser('~/.surprise_data/ml-100k/ml-100k/u.item'),
+                                   sep='|', encoding='ISO-8859-1', header=None,
+                                   names=['mid', 'mtitle'] + [x for x in range(22)])
+        # 每列都转换为字符串类型
+        self.data_df = self.data_df.astype(str)
+        self.item_df = self.item_df.astype(str)
+        # 电影id到电影标题的映射
+        self.item_dict = {self.item_df.loc[x, 'mid']: self.item_df.loc[x, 'mtitle'] for x in range(len(self.item_df))}
+        self.user_based_sim_option = {'name': 'pearson_baseline', 'user_based': True}
+        # item-based
+        self.item_based_sim_option = {'name': 'pearson_baseline', 'user_based': False}
+
+    def getDictKey(self, Dict, Value):
+        return [k for k, v in Dict.items() if v == Value]
+
+    def item_to_movie_name(self, item):
+        return self.item_dict[item]
+
+    def movie_name_to_item(self, movie_name):
+        movies_list = self.getDictKey(self.item_dict, movie_name)
+        return movies_list[0]
+
+    def seconds_to_ctime(self, seconds):
+        seconds = seconds if isinstance(seconds, int) else int(seconds)
+        return time.ctime(seconds)
+
+    # search for the rating history of user with uid
+    def getting_user_history(self, uid):
+        uid = uid if isinstance(uid, str) else str(uid)
+
+        self.data_df_sorted = self.data_df.sort_values(by=["user", "timestamp"], ascending=[True, True])
+        data_user = self.data_df_sorted.loc[self.data_df["user"] == uid]
+
+        for idx, row in data_user.iterrows():
+            row["item"] = self.item_to_movie_name(row["item"])
+            row["timestamp"] = self.seconds_to_ctime(row["timestamp"])
+            print("{}, {}, {}, {}".format(idx, row["item"], row["rating"], row["timestamp"]))
+
+        return data_user
+
+    # unlearning request for movie name
+    def unlearning_request(self, history_index, movie_index):
+        if not isinstance(history_index, int):
+            print("input int type index the input type is {}".format(type(history_index)))
+            return ValueError
+
+        self.data_df_droped = self.data_df.drop([history_index], axis=0)
+        self.data_df_droped.to_csv("../data/u-unlearning{}.csv".format(history_index), sep="\t", header=None,
+                                   index=False)
+        print("user requested to forget history of movie: {}".format(self.item_to_movie_name(movie_index)))
+        print("---- forgetting request done! -----")
+
+    # train model
+    def train_model(self):
+        trainset = self.data.build_full_trainset()
+        algo = KNNBaseline(sim_option=self.user_based_sim_option)
+        algo.fit(trainset)
+
+        return algo
+
+    # 为用户推荐n部电影，基于用户的协同过滤算法，先获取10个相似度最高的用户，把这些用户评分高的电影加入推荐列表。
+    def get_similar_users_recommendations(self, algo, uid, n=10):
+        # 将原始id转换为内部id
+        inner_id = algo.trainset.to_inner_uid(uid)
+        # 使用get_neighbors方法得到10个最相似的用户
+        neighbors = algo.get_neighbors(inner_id, k=10)
+        neighbors_uid = (algo.trainset.to_raw_uid(x) for x in neighbors)
+        recommendations = set()
+        # 把评分为5的电影加入推荐列表
+        for user in neighbors_uid:
+            if len(recommendations) > n:
+                break
+            item = self.data_df[self.data_df['user'] == user]
+            item = item[item['rating'] == '5']['item']
+            for i in item:
+                recommendations.add(self.item_dict[i])
+        print('\nrecommendations for user %s:' % uid)
+        for cnt, movie_name in enumerate(list(recommendations)):
+            if cnt >= 10:
+                break
+            print(movie_name)
+
+    def train_unlearning_model_crossvalidate(self):
+        trainset = self.data.build_full_trainset()
+        algo = KNNBaseline(sim_option=self.user_based_sim_option)
+
+        validatation_res = cross_validate(algo, self.data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+        test_rmse = validatation_res['test_rmse']
+        test_mae = validatation_res['test_mae']
+        rmse_mean = sum(test_rmse) / len(test_rmse)
+        mae_mean = sum(test_mae) / len(test_mae)
+
+        algo.fit(trainset)
+
+        return algo, rmse_mean, mae_mean
+
+    def recommendation_unlearning(self, forgetting_index, validate_flag):
+        path_to_u_data = "../data/u-unlearning{}.csv".format(forgetting_index)
+        self.data_readin(path_to_u_data)
+        print(self.data_df)
+        # uid = input("input the user who wanna forget his/her history in recommendation system\n")
+        uid = random.randint(0, self.user_total_num)
+        print("----- select user uid {} -----".format(uid))
+        data_user = self.getting_user_history(uid)
+        print(data_user)
+        indexarr = data_user.index.values
+
+        # history_index = input("input the movie ranking history index user wanna forget\n")
+        # history_index = int(history_index)
+        history_index = random.choice(indexarr).item()
+        dest = data_user.loc[history_index]
+        movie_name = dest["item"]
+        movie_index = self.movie_name_to_item(movie_name)
+
+        self.unlearning_request(history_index, movie_index)
+
+        if validate_flag:
+            alg, rmse, mae = self.train_unlearning_model_crossvalidate()
+            self.RMSE.append(rmse)
+            self.MAE.append(mae)
+        else:
+            alg = self.train_model()
+
+        # get_similar_users_recommendations(alg, uid, 10)
+        return history_index
---
+++ score/score_using_knn.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+from KNNbaseline import KNNbase_Unlearning
+
+
+if __name__ == "__main__":
+
+    KNN_Unl = KNNbase_Unlearning()
+    data_df = KNN_Unl.data_readin('~/.surprise_data/ml-100k/ml-100k/u.data')
+    algorithm = KNN_Unl.train_model()
+    KNN_Unl.get_similar_users_recommendations(algorithm, '7', 10)
+    KNN_Unl.data_df.to_csv("../data/u-unlearning0.csv", sep="\t", header=None, index=False)
+
+    time_start = time.time()
+    next_index = KNN_Unl.recommendation_unlearning(0, True)
+    for i in range(50):
+        validate_flag = False
+        if i % 10 == 9:
+            validate_flag = True
+        print("---- trainning rounds {} ----".format(i))
+        print('----- time elapsed {} s -----'.format(time.time() - time_start))
+        next_index = KNN_Unl.recommendation_unlearning(next_index, validate_flag)
+
+    res = {'RMSE': KNN_Unl.RMSE, 'MAE': KNN_Unl.MAE}
+    res_data = pd.DataFrame(res)
+    res_data.to_csv('../results/RMSE_MAE_res.cvs')
+
+    x = range(0, 60, 10)
+    plt.plot(x, KNN_Unl.RMSE, label='RMSE', linewidth=1, color='r', marker='o')
+    # markerfacecolor='blue',markersize=12)
+    plt.plot(x, KNN_Unl.MAE, label='MAE', linewidth=1, color='b', marker='*')
+    plt.xlabel('unlearning data points')
+    plt.ylabel('error')
+    plt.title('RMSE and MAE\nfor machine unlearning')
+    plt.legend()
+    plt.show()
+
